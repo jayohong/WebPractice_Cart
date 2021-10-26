87a67955a2cabf5785f513e56a304e87a1e6fdaa
jayohong <88691@LAPTOP-COBPTJB9>
2021-10-26 09:23:11 +0800

    nothing

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,23 +1,28 @@
 package dao;
 
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import entity.User;
 
-public class UserDao {
-	private static List<User> users;
+public class UserDao extends BaseDao{
+	//private static List<User> users;
 	
-	static {
-		users = new ArrayList<User>();
-		users.add(new User(1,"admin","1111"));
-		users.add(new User(1,"John","1111"));
-		users.add(new User(1,"Mary","1111"));
-	}
+//	static {
+//		users = new ArrayList<User>();
+//		users.add(new User(1,"admin","1111"));
+//		users.add(new User(1,"John","1111"));
+//		users.add(new User(1,"Mary","1111"));
+//	}
+//	
 	
-	public User loginCheck(String name, String password) {
-		return users.stream().filter(u -> u.getName().equals(name) && u.getPassword().equals(password))
-				.findFirst().get(); 
-	}
+	
+
+	
+//	public User loginCheck(String name, String password) {
+//		return users.stream().filter(u -> u.getName().equals(name) && u.getPassword().equals(password))
+//				.findFirst().get(); 
+//	}
 	
 }
